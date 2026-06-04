bfa657361c3712646ede85815a90168162c81dcd
Nick Baker <nickbaker9941@gmail.com>
2026-06-04 01:43:59 -0700

    gamma-correct colors

--- color.h
+++ color.h
@@ -6,10 +6,17 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    return (linear_component > 0) ? std::sqrt(linear_component) : 0;
+}
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     static const interval intensity(0.000, 0.999);
     int rbyte = int(255.999 * intensity.clamp(r));
