bde0a0a2daca0801531eae8893a3460b3829982d
Nick Baker <nickbaker9941@gmail.com>
2026-06-04 02:05:19 -0700

    add materials: matte surface & metal reflective

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
   public:
@@ -89,8 +90,10 @@ class camera {
             return color(0, 0, 0);
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
--- sphere.h
+++ sphere.h
@@ -5,7 +5,7 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat) : center(center), radius(std::fmax(0,radius)), mat(mat) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
@@ -29,6 +29,7 @@ class sphere : public hittable {
 
         rec.t = root;
         rec.p = r.at(rec.t);
+        rec.mat = mat;
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
 
@@ -38,6 +39,7 @@ class sphere : public hittable {
   private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
